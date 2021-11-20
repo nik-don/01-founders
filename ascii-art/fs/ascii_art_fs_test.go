5a511ee29457456201d2145b8a0f819462b760b8
Nik <93073558+nik-don@users.noreply.github.com>
2021-11-20 23:17:40 +0000

    Update ascii_art_fs_test.go

--- ascii-art/fs/ascii_art_fs_test.go
+++ ascii-art/fs/ascii_art_fs_test.go
@@ -1,9 +1,7 @@
 /* Package contains the functions required to compare the command line output
 with the expected result stored in a file.
 
-It also makes two additional checks. (empty input and only "\n")
-
-https://github.com/nik-don/01-founders/tree/main/ascii-art
+https://github.com/nik-don/01-founders/tree/main/ascii-art/fs
 */
 package main
 
